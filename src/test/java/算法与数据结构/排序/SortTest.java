2e32216f756373598bf0ae371101721bd4ba9975
kangkang.lv <kangkang.lv@dajie-inc.com>
2018-04-12 11:08:26 +0800

    排序算法

--- src/test/java/算法与数据结构/排序/SortTest.java
+++ src/test/java/算法与数据结构/排序/SortTest.java
@@ -68,32 +68,13 @@ public class SortTest {
             for (int i = step;i<arr.length;i+=step){
                 temp = arr[i];
                 int j = i;
-                for (;j >= step && temp < arr[j-step];j-=step){
+                for (;j > 0 && temp < arr[j-step];j-=step){
                     arr[j] = arr[j-step];
                 }
                 arr[j] = temp;
             }
         }
     }
-
-    public static void sort1(int []arr){
-                 //增量gap，并逐步缩小增量
-                 for(int gap=arr.length/2;gap>0;gap/=2){
-                         //从第gap个元素，逐个对其所在组进行直接插入排序操作
-                         for(int i=gap;i<arr.length;i++){
-                                 int j = i;
-                                 int temp = arr[j];
-                                 if(arr[j]<arr[j-gap]){
-                                          while(j-gap>=0 && temp<arr[j-gap]){
-                                                 //移动法
-                                                 arr[j] = arr[j-gap];
-                                                 j-=gap;
-                                             }
-                                         arr[j] = temp;
-                                     }
-                             }
-                     }
-             }
 
     /**
      * 冒泡排序
@@ -181,5 +162,6 @@ public class SortTest {
         arr[a] = arr[b];
         arr[b] = temp;
     }
+
 
 }
